4012e60c9a79fe0cda088f7d792564540055d741
DivineDemon <supame123@gmail.com>
2024-07-22 02:35:29 +0400

    Uploadthing + Prisma Configuration!

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
+import sharp from "sharp";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { db } from "@/db";
 
 const f = createUploadthing();
 
@@ -13,9 +15,32 @@ export const ourFileRouter = {
     .middleware(async ({ input }) => {
       return { input };
     })
-    .onUploadComplete(async ({ metadata }) => {
+    .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
-      return { configId };
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            width: width || 500,
+            height: height || 500,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: { id: configId },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
